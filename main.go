2d869d4cb870840488f4e3156a596aa0d683f826
delaemon <takeshi.music.5@gmail.com>
2017-05-03 22:07:58 +0900

    First commit

---
+++ main.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	MQTT "github.com/eclipse/paho.mqtt.golang"
+)
+
+const MQTT_BROKER = "tcp://localhost:1883"
+
+func createMQTTClient(brokerAddr, clientId, username, password string) MQTT.Client {
+	opts := MQTT.NewClientOptions().AddBroker(brokerAddr)
+	opts.SetClientID(clientId)
+	opts.SetUsername(username)
+	opts.SetPassword(password)
+	client := MQTT.NewClient(opts)
+	return client
+}
+
+func subscribe(client MQTT.Client, sub chan<- MQTT.Message) {
+	fmt.Println("start subscribing...")
+	subToken := client.Subscribe(
+		"path/to/topic",
+		0,
+		func(client MQTT.Client, msg MQTT.Message) {
+			sub <- msg
+		})
+	if subToken.Wait() && subToken.Error() != nil {
+		fmt.Println(subToken.Error())
+		os.Exit(1)
+	}
+}
+
+func publish(client MQTT.Client, input string) {
+	token := client.Publish("path/to/topic", 0, true, input)
+	if token.Wait() && token.Error() != nil {
+		panic(token.Error())
+	}
+}
+
+func input(pub chan<- string) {
+	for {
+		var input string
+		fmt.Scanln(&input)
+		pub <- input
+	}
+}
+
+func main() {
+	fmt.Print("your id: ")
+	var id string
+	fmt.Scanln(&id)
+	client := createMQTTClient(MQTT_BROKER, id, "YourID", "YouPassword")
+	defer client.Disconnect(250)
+	if token := client.Connect(); token.Wait() && token.Error() != nil {
+		panic(token.Error())
+	}
+	sub := make(chan MQTT.Message)
+	go subscribe(client, sub)
+	pub := make(chan string)
+	go input(pub)
+	for {
+		select {
+		case s := <-sub:
+			msg := string(s.Payload())
+			fmt.Printf("\nmsg: %s\n", msg)
+		case p := <-pub:
+			publish(client, p)
+		}
+	}
+}
